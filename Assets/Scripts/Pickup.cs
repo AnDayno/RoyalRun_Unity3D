a4286e121f4b830257d4f2d667ee39cba1e19113
An <162073486+AnDayno@users.noreply.github.com>
2025-09-28 19:58:03 +0800

    Added collision detection to gameobjects and player

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    const string PLAYER_TAG = "Player";
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(PLAYER_TAG))
+        {
+            Debug.Log("Picked up by: " + other.gameObject.name);
+        }
+    }
+}
