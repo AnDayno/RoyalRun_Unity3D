f008a4ea233a2ae98041b9d4be9e57113373532b
An <162073486+AnDayno@users.noreply.github.com>
2025-09-30 17:58:04 +0800

    Added stumble animation trigger

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -1,9 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown = 1f;
+
+    float cooldownTimer = 0f;
+
+    const string hitTrigger = "Hit";
+
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collided with: " + collision.gameObject.name);
+        if (cooldownTimer < collisionCooldown) return;
+
+            animator.SetTrigger(hitTrigger);
+            cooldownTimer = 0f;  
     }
 }
