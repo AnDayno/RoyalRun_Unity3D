511fbb1f69d38564ad3283f5e2c97390298b0493
An <162073486+AnDayno@users.noreply.github.com>
2025-10-01 01:36:51 +0800

    Added Inheritance to apple and coin

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] float rotationSpeed = 100f;
     const string PLAYER_TAG = "Player";
+
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(PLAYER_TAG))
         {
-            Debug.Log("Picked up by: " + other.gameObject.name);
+            OnPickup();
+            Destroy(gameObject);
         }
     }
+
+    protected abstract void OnPickup();
 }
